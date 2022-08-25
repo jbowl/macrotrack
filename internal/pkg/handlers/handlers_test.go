fa81d86793b0e867f304eaa0931f54f712e674da
j <joel4268@gmail.com>
2022-08-25 18:16:47 -0400

    TestCRUD method

--- internal/pkg/handlers/handlers_test.go
+++ internal/pkg/handlers/handlers_test.go
@@ -2,10 +2,12 @@ package handlers
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
 	"macrotrack/internal/pkg/store"
+	"macrotrack/internal/pkg/types"
 	"net/http"
 	"net/http/httptest"
 	"os"
@@ -25,10 +27,16 @@ func TestMain(m *testing.M) {
 
 	//store.DSN = fmt.Sprintf("user id=%s;password=%s;port=%s;database=%s", store.User, store.Password, store.Port, store.Database)
 
+	path, err := os.Getwd()
+	if err != nil {
+		log.Println(err)
+	}
+	fmt.Println(path) // for example /home/user
+
 	cmdString := `./ctr.sh`
 	cmd := exec.Command(cmdString)
 
-	err := cmd.Run()
+	err = cmd.Run()
 
 	if err != nil {
 		log.Fatalf("unable to start test container %v", err)
@@ -42,6 +50,122 @@ func TestMain(m *testing.M) {
 	exitVal := m.Run()
 
 	os.Exit(exitVal)
+
+}
+
+func Create(c types.Macro) *http.Response {
+	body, _ := json.Marshal(c)
+
+	r := httptest.NewRequest(http.MethodPost, "/macros", bytes.NewBuffer(body))
+	r.Header.Set("Content-Type", "application/json")
+	w := httptest.NewRecorder()
+
+	CreateMacroHandler(dataStore, w, r)
+
+	return w.Result()
+}
+
+func Read(target string) *http.Response {
+
+	r := httptest.NewRequest(http.MethodGet, target, nil)
+	w := httptest.NewRecorder()
+
+	ReadMacroHandler(dataStore, w, r)
+	return w.Result()
+
+}
+
+func Update(target string, u types.Macro) *http.Response {
+
+	body, _ := json.Marshal(u)
+
+	r := httptest.NewRequest(http.MethodPut, target, bytes.NewBuffer(body))
+	r.Header.Set("Content-Type", "application/json")
+
+	w := httptest.NewRecorder()
+
+	//	res := Read(target)
+
+	UpdateMacroHandler(dataStore, w, r)
+
+	return w.Result()
+}
+
+func TestCRUD(t *testing.T) {
+
+	createData := types.Macro{Carbs: 100, Protein: 99, Fat: 98, Alcohol: 97}
+	res := Create(createData)
+
+	if res.StatusCode != 201 {
+		res.Body.Close()
+		t.Errorf("expected Status == 201  error got %v", res.StatusCode)
+	}
+
+	res.Body.Close()
+
+	h := res.Header.Get("location")
+	path := h[11:]
+
+	readRes := Read(path)
+
+	defer readRes.Body.Close()
+
+	//	var fileInfo stagingfile
+	//	data, err := io.ReadAll(readRes.Body)
+
+	var readData types.Macro
+
+	//err := json.Unmarshal(readRes.Body, &readData)
+	err := json.NewDecoder(readRes.Body).Decode(&readData)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// check header
+
+	if readRes.StatusCode != http.StatusOK {
+		fmt.Println(readRes.StatusCode)
+	}
+
+	// compare all fields
+	if createData.Carbs != readData.Carbs {
+
+	}
+
+	u := types.Macro{Carbs: 50, Protein: 50, Fat: 98, Alcohol: 97}
+
+	uRes := Update(path, u)
+
+	var updateData types.Macro
+
+	//err := json.Unmarshal(readRes.Body, &readData)
+	err = json.NewDecoder(uRes.Body).Decode(&updateData)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	h2 := uRes.Header.Get("location")
+	path2 := h2[11:]
+
+	readRes2 := Read(path2)
+
+	defer readRes2.Body.Close()
+
+	//	var fileInfo stagingfile
+	//	data, err := io.ReadAll(readRes.Body)
+
+	var updatedData types.Macro
+
+	//err := json.Unmarshal(readRes.Body, &readData)
+	err = json.NewDecoder(readRes2.Body).Decode(&updatedData)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// now update
 
 }
 
@@ -80,9 +204,7 @@ func TestCreateMacro(t *testing.T) {
 	//	}
 
 	h := res.Header.Get("location")
-
 	path := h[11:]
-
 	req = httptest.NewRequest(http.MethodGet, path, nil)
 
 	w2 := httptest.NewRecorder()
