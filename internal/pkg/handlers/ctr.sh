62985fdf65e055c9f3371de0a346a33f4ca5084a
j <joel4268@gmail.com>
2022-08-25 18:26:48 -0400

    container load

--- internal/pkg/handlers/ctr.sh
+++ internal/pkg/handlers/ctr.sh
@@ -5,8 +5,7 @@
 
 output=$( podman ps -a -f name=sqlserver_test | grep sqlserver_test 2> /dev/null )
 if [[ -z ${output} ]]; then 
-  podman run -d -e "ACCEPT_EULA=Y" -e "MSSQL_SA_PASSWORD=___Aa123" --name sqlserver_test --hostname sqlserver_test --rm -p 1434:1433 mcr.microsoft.com/mssql/server:2019-latest
-else  
+  podman run -d -e "ACCEPT_EULA=Y" -e "MSSQL_SA_PASSWORD=___Aa123" --name sqlserver_test --hostname sqlserver_test --rm -p 1434:1433 mcr.microsoft.com/mssql/server:2019-latest 
 fi
 
 
